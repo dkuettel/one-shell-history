2917ce87625c97fa16cf125bfc17654fef81a62a
Daniel Kuettel <dkuettel@gmail.com>
2020-09-28 00:19:48 +0200

    draft more robust git sync

--- python/osh/sync/git.py
+++ python/osh/sync/git.py
@@ -21,43 +21,70 @@ def get_repo():
     return repo
 
 
+def sync():
+
+    from pathlib import Path
+
+    import osh.history
+
+    repo = get_repo()
+    try:
+        repo.remotes["origin"].fetch()
+    except IndexError:
+        print("git sync uses the repository at ~/.one-shell-history/sync/git")
+        print(
+            "make sure that repo is set up to push to and pull from a remote 'origin'"
+        )
+        print("for master branch tracking a branch on that remote")
+        print("dont forget to set core.sshCommand if you need special ssh keys")
+        print("often first: 'git remote add origin git@github.com:someone/somewhere.git'")
+        print("maybe then: 'git config core.sshCommand 'ssh -i ~/.ssh/your-key'")
+        print("and then: 'git push -u origin master'")
+        exit(1)
+    repo.head.reset(hard=True)
+
+    remote_file = Path("~/.one-shell-history/sync/git/zsh-history.json").expanduser()
+    remote_history = osh.history.read_from_file(remote_file, or_empty=True)
+
+    local_file = Path("zsh-history.json")
+    local_history = osh.history.read_from_file(local_file, or_empty=True)
+
+    merged = osh.history.merge([local_history, remote_history])
+
+    if merged == local_history:
+        print("no incoming changes")
+    else:
+        print("incoming changes")
+        osh.history.write_to_file(merged, local_file)
+
+    if merged == remote_history:
+        print("no outgoing changes")
+    else:
+        print("outgoing changes")
+        osh.history.write_to_file(merged, remote_file)
+        repo.index.add([str(remote_file)])
+        repo.index.commit("sync")
+        repo.remotes["origin"].push()
+
+
 @click.group()
 def cli():
     pass
 
 
 @cli.command()
-def setup():
-    get_repo()
+def sync_now():
+    sync()
 
 
 @cli.command()
-def sync():
+def sync_always(interval=10 * 60):
 
-    from pathlib import Path
+    import time
 
-    import osh.history as H
-
-    repo = get_repo()
-    repo.remotes["origin"].fetch()
-    repo.head.reset(hard=True)
-
-    local_history_file = Path("./zsh-history.json")
-    local_history = H.read_from_file(local_history_file, or_empty=True)
-
-    remote_history_file = (
-        Path("~/.one-shell-history/sync/git").expanduser() / "zsh-history.json"
-    )
-    remote_history = H.read_from_file(remote_history_file)
-
-    merged_history = H.merge([local_history, remote_history])
-
-    H.write_to_file(merged_history, local_history_file)
-    H.write_to_file(merged_history, remote_history_file)
-
-    repo.index.add([str(remote_history_file)])
-    repo.index.commit("sync")
-    repo.remotes["origin"].push()
+    while True:
+        sync()
+        time.sleep(interval)
 
 
 if __name__ == "__main__":
