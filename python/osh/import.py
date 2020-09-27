f507b3b549dac655895f4577ae763a146e6ef381
Daniel Kuettel <dkuettel@gmail.com>
2020-09-27 23:01:51 +0200

    robustly merge parallel running zsh and osh histories

--- python/osh/import.py
+++ python/osh/import.py
@@ -1,27 +1,21 @@
 from pathlib import Path
 import re
 import datetime
+from typing import List, Optional
 
 import click
 
 import osh.history
+from osh.history import Event
 
 
-@click.group()
-def cli():
-    pass
+def read_zsh_history(file: Path, machine: Optional[str] = None) -> List[Event]:
 
-
-@cli.command()
-@click.option("--location", default="~/.zsh_history")
-@click.option("--machine", default=None)
-def zsh(location, machine):
-
-    location = Path(location).expanduser()
     pattern = re.compile(r"^: (?P<timestamp>\d+):(?P<duration>\d+);(?P<command>.*)$")
+
     history = []
     zsh_history = enumerate(
-        location.read_text(encoding="utf-8", errors="replace").split("\n")[:-1], start=1
+        file.read_text(encoding="utf-8", errors="replace").split("\n")[:-1], start=1
     )
 
     for line, content in zsh_history:
@@ -33,22 +27,66 @@ def zsh(location, machine):
             int(match.group("timestamp")), tz=datetime.timezone.utc
         )
         command = match.group("command")
-        duration = int(match.group("duration"))
+        # note: duration in my zsh version 5.8 doesnt seem to be recorded correctly, its always 0
+        # duration = int(match.group("duration"))
         while command.endswith("\\"):
             line, content = next(zsh_history)
             command = command[:-1] + "\n" + content
-        event = osh.history.Event(
-            timestamp=timestamp, command=command, duration=duration, machine=machine
-        )
+        event = Event(timestamp=timestamp, command=command, machine=machine)
         history.append(event)
 
-    history = osh.history.make(history)
-    previous = osh.history.read_from_file(Path("imported.json"))
-    merged = osh.history.merge([history, previous])
-    osh.history.write_to_file(merged, Path("imported.json"))
+    return osh.history.make(history)
 
-    print(f"{len(set(history)-set(previous))} new events")
-    print(f"{len(set(previous)-set(history))} other events")
+
+def merge_zsh_into_osh(
+    zsh_history: List[Event], osh_history: List[Event], timestamp_slack: float = 1.5
+) -> List[Event]:
+    """ merge a zsh history into an osh history
+    taking special care to match double entries
+        a) previously imported entries are trivial, they are exactly the same
+        b) recorded by both zsh and osh are more difficult and matched with a heuristic
+    """
+
+    def is_collision(a, b):
+        # todo do we record the command string exactly the same? escaping and all that?
+        return (
+            (a.command == b.command)
+            and (a.machine == b.machine)
+            and (abs((a.timestamp - b.timestamp).total_seconds()) < timestamp_slack)
+        )
+
+    merged = list(osh_history)
+    for candidate in zsh_history:
+        if not any(is_collision(candidate, event) for event in osh_history):
+            merged.append(candidate)
+
+    return osh.history.make(merged)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("--zsh-file", default="~/.zsh_history")
+@click.option("--machine", required=True)
+@click.option("--osh-file", default="zsh-history.json")
+def zsh(zsh_file, machine, osh_file):
+
+    zsh_file = Path(zsh_file).expanduser()
+    osh_file = Path(osh_file).expanduser()
+
+    zsh_history = read_zsh_history(file=zsh_file, machine=machine)
+    osh_history = osh.history.read_from_file(osh_file)
+
+    merged = merge_zsh_into_osh(zsh_history=zsh_history, osh_history=osh_history)
+
+    print(
+        f"{len(merged)-len(osh_history)} out of {len(zsh_history)} in zsh are new to osh"
+    )
+
+    osh.history.write_to_file(merged, osh_file)
 
 
 if __name__ == "__main__":
