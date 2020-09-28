11554647fa90d971ff16906d01af5fa6dc9fa458
Daniel Kuettel <dkuettel@gmail.com>
2020-09-28 22:25:04 +0200

    add simple lockfile handling

---
+++ python/osh/utils.py
@@ -0,0 +1,54 @@
+from contextlib import contextmanager
+from pathlib import Path
+import os
+from typing import Optional
+import time
+import math
+import random
+
+
+class NoLock(Exception):
+    pass
+
+
+@contextmanager
+def locked_file(file: Path, wait: Optional[float] = None, forever: bool = False):
+    """ lazy lockfile implementation, not sure if absolutely failsafe
+    file is the original file to be locked, ".lock" is appended automatically
+    """
+
+    # this might produce race conditions?
+    file.parent.mkdir(parents=True, exist_ok=True)
+
+    lfile = f"{file}.lock"
+
+    if forever:
+        assert wait is None
+        wait = math.inf
+
+    starttime = time.time()
+    fd = None
+    while fd is None:
+        try:
+            fd = os.open(path=lfile, flags=os.O_CREAT | os.O_EXCL)
+        except FileExistsError:
+            if wait is None:
+                raise NoLock()
+            elif (time.time() - starttime) > wait:
+                raise NoLock()
+            time.sleep(random.uniform(0.5, 1.0))
+
+    try:
+        yield
+
+    finally:
+
+        try:
+            os.unlink(lfile)
+        except:
+            pass
+
+        try:
+            os.close(fd)
+        except:
+            pass
