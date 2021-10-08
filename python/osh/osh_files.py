fac4cfc707cd5119dab20b574fb426b9a9255a5d
Daniel Kuettel <dkuettel@gmail.com>
2021-10-08 19:40:19 +0200

    read osh files more incrementally

---
+++ python/osh/osh_files.py
@@ -0,0 +1,147 @@
+import datetime
+import json
+from pathlib import Path
+from typing import Generator, Iterable
+
+from osh.history import Event
+
+""" the osh file format
+json lines
+file extension is osh, eg, history.osh
+file only grows in lines, should make it easy to append and read when watching
+only lines with a close new-line are ready to be read
+entries contain any of those keys
+    format, description, event
+all but event overwrite a previous setting, event appends to events
+a new format takes effect already in the current line
+event contains the keys
+    timestamp, command, duration, exit-code, folder, machine, session
+the only format currently is "osh-history-v1"
+"""
+
+
+def create_osh_file(file: Path):
+    file = file.expanduser()
+    header = {
+        "format": "osh-history-v1",
+        "description": None,
+    }
+    file.write_text(json.dumps(header) + "\n")
+
+
+def read_osh_file(file: Path):
+    return [event for event in OshFileReader(file).read_events()]
+
+
+def append_event_to_osh_file(file: Path, event: Event):
+    file = file.expanduser()
+    json_str = json.dumps({"event": event_to_json_dict(event)})
+    with file.open("at") as f:
+        f.write(json_str + "\n")
+
+
+class FileChangedMuch(Exception):
+    pass
+
+
+class OshFileReader:
+    def __init__(self, file: Path):
+        self.file = file
+        self.last_file = None
+        self.last_mtime = None
+        self.last_size = None
+        self.last_tell = None
+        self.last_line = None
+
+    def read_events(self) -> Generator[Event]:
+
+        file = self.file.resolve()
+        stat = file.stat()
+
+        # TODO is it more robust to open the file and work on the file descriptor?
+        # this way we for sure read the content of the stats we just checked
+
+        if self.last_file is None:
+            self.last_file = file
+            self.last_mtime = stat.st_mtime
+            self.last_size = stat.st_size
+            self.last_tell = 0
+            self.last_line = None
+            return self._generate()
+
+        if self.last_file != self.file:
+            raise FileChangedMuch()
+
+        if self.last_mtime != stat.mtime and self.last_size < stat.st_size:
+            self.last_mtime = stat.st_mtime
+            self.last_size = stat.st_size
+            return self._generate()
+
+        if self.last_mtime != stat.mtime and self.last_size >= stat.st_size:
+            raise FileChangedMuch()
+
+        def nothing():
+            yield from []
+
+        return nothing()
+
+    def _generate(self):
+
+        file_format = "osh-history-v1"
+
+        with self.last_file.open("rt") as file:
+
+            file.seek(self.last_tell)
+            if self.last_line is not None:
+                if self.last_line != file.readline():
+                    raise FileChangedMuch()
+
+            for line in file:
+                if not line.endswith("\n"):
+                    return
+
+                self.last_tell = file.tell()
+                self.last_line = line
+
+                line = json.loads(line)
+
+                file_format = line.pop("format", file_format)
+                assert file_format == "osh-history-v1"
+
+                line.pop("description", None)
+
+                if "event" in line:
+                    yield event_from_json_dict(line.pop("event"))
+
+                if len(line) > 0:
+                    raise Exception(f"unexpected content left: {line}")
+
+
+def event_to_json_dict(event: Event) -> dict:
+    jd = dict()
+    jd["timestamp"] = event.timestamp.isoformat(timespec="microseconds")
+    jd["command"] = event.command
+    assert event.duration is not None
+    jd["duration"] = event.duration
+    assert event.exit_code is not None
+    jd["exit-code"] = event.exit_code
+    assert event.folder is not None
+    jd["folder"] = event.folder
+    assert event.machine is not None
+    jd["machine"] = event.machine
+    assert event.session is not None
+    jd["session"] = event.session
+    return jd
+
+
+def event_from_json_dict(jd: dict) -> Event:
+    # TODO sanity check that nothing is None?
+    return Event(
+        timestamp=datetime.datetime.fromisoformat(jd["timestamp"]),
+        command=jd["command"],
+        duration=jd["duration"],
+        exit_code=jd["exit-code"],
+        folder=jd["folder"],
+        machine=jd["machine"],
+        session=jd["session"],
+    )
