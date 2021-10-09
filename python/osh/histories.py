ea9992a54f1a68d79dfc10f7209c6cd5af01daa0
Daniel Kuettel <dkuettel@gmail.com>
2021-10-09 20:10:06 +0200

    load histories from folder structure

--- python/osh/histories.py
+++ python/osh/histories.py
@@ -1,70 +1,83 @@
+import itertools
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Callable, Optional, Tuple
+from typing import Callable, Generator, Optional, Tuple
 
-from osh.osh_file import OshFile
+from osh.history import Event
 from osh.sinks import OshSink, Sink
-from osh.sources import OshLegacySource, OshSource, Source, UnionSource, ZshSource
+from osh.sources import (
+    MergeInSource,
+    OshLegacySource,
+    OshSource,
+    Source,
+    UnionSource,
+    ZshSource,
+)
 
 
-def histories_from_folder_structure(
-    basefolder: Path, sink_path: Path
-) -> Tuple[Sink, Source]:
-    """return a single sink and source (usually a Union) to work with as a data backend
-    basefolder will be created if it does not exist
-    any histories found will be loaded based on extension
-    basefolder/archive and subfolders contains anything that is not active
-    they will be loaded but not watched for file content changes
-    new file additions to basefolder/archive however will be picked up
-    "*.osh" files directly in basefolder root are considered to be active and they are watched for change
-    non-"*.osh" files in root are ignored
-    sink_path is relative to basefolder, this is where new events are appended to
-    do not have two different instances (local or remote) of osh write to the same osh sink file
-    sink_path is initialized empty if it does not exist
-    """
+def source_from_folder_structure(base: Path) -> Source:
 
-    osh_file = OshFile(basefolder / sink_path)
-    if not osh_file.exists():
-        osh_file.create()
-
-    osh_sink = OshSink(osh_file)
-    osh_source = OshSource(osh_file)
-
-    # TODO we dont watch yet in case archive gets new files added (not file content changes though)
-    # simplest implementation just reloads completely on potential changes
-    sources = (
-        [osh_source]
-        + discover_active_osh_sources(basefolder, ignore=basefolder / sink_path)
-        + discover_archived_osh_sources(basefolder)
+    active_osh_sources = DiscoveredSources(lambda: base.glob("*.osh"))
+    archived_osh_sources = DiscoveredSources(
+        lambda: itertools.chain(
+            base.glob("archive/**/*.osh"),
+            base.glob("archive/**/*.osh_legacy"),
+        )
     )
-    merge_sources = discover_archived_other_sources(basefolder)
+    archived_other_sources = DiscoveredSources(
+        lambda: itertools.chain(
+            base.glob("archive/**/*.zsh_history"),
+        )
+    )
 
-    # TODO maybe we need to cascade unions differently to efficiently handle updates and changes
-    full_source = UnionSource(sources=sources, merge_sources=merge_sources)
+    osh_sources = UnionSource([active_osh_sources, archived_osh_sources])
+    source = MergeInSource(main=osh_sources, other=archived_other_sources)
 
-    return osh_sink, full_source
+    # TODO this it the minimal thing that is correct, but not efficient
+    # eventually: splits, mtime logic, efficient merging early and behind an mtime cache
 
-
-def discover_active_osh_sources(
-    basefolder: Path, ignore: Optional[Path] = None
-) -> list[OshSource]:
-    return [
-        OshSource(OshFile(file)) for file in basefolder.glob("*.osh") if file != ignore
-    ]
-
-
-def discover_archived_osh_sources(basefolder: Path) -> list[Source]:
-    return [
-        OshSource(OshFile(file)) for file in basefolder.glob("archive/**/*.osh")
-    ] + [OshLegacySource(file) for file in basefolder.glob("archive/**/*.osh-legacy")]
+    return source
 
 
-def discover_archived_other_sources(basefolder: Path) -> list[Source]:
-    return [ZshSource(file) for file in basefolder.glob("archive/**/*.zsh_history")]
+class DiscoveredSources(Source):
+    def __init__(self, discover):
+        self.sources: dict[Path, Source] = {}
+        self.union = UnionSource([])
+        self.discover = discover
+
+    def as_list(self) -> list[Event]:
+        self.maybe_refresh()
+        return self.union.as_list()
+
+    def maybe_refresh(self):
+        new_sources = {}
+        for path in self.discover():
+            if path in self.sources:
+                new_sources[path] = self.sources[path]
+            else:
+                try:
+                    new_sources[path] = make_source(path)
+                except FileNotFoundError:
+                    pass
+        if set(self.sources) == set(new_sources):
+            return
+        self.sources = new_sources
+        self.union = UnionSource(list(self.sources.values()))
+
+
+def make_source(path: Path) -> Source:
+    known = {
+        ".osh": OshSource,
+        ".osh_legacy": OshLegacySource,
+        ".zsh_history": ZshSource,
+    }
+    if path.suffix not in known:
+        raise Exception(f"unknown suffix in {path}")
+    return known[path.suffix](path)
 
 
 if __name__ == "__main__":
-    sink, source = histories_from_folder_structure(Path("histories"), Path("base.osh"))
+    source = source_from_folder_structure(Path("histories"))
     events = source.as_list()
     print(f"{len(events)=}")
     print(f"{events[-1]=}")
