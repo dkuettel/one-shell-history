931a2a489496c1f67663bffb9e9e1a0cbc8e8b51
Daniel Kuettel <dkuettel@gmail.com>
2021-10-11 22:13:20 +0200

    try sources.py way smaller and a bit more incremental and pragmatic

--- python/osh/sources.py
+++ python/osh/sources.py
@@ -1,23 +1,140 @@
 from __future__ import annotations
 
 import datetime
+import itertools
 import json
 import math
 import re
 import time
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Optional
+from typing import Callable, Optional, Tuple
 
 from osh.history import Event
 from osh.osh_files import (
-    FileChangedMuch,
     OshFileReader,
     create_osh_file,
     read_osh_file,
     read_osh_legacy_file,
 )
 from osh.zsh_files import read_zsh_file
+
+
+class IncrementalSource:
+    """
+    the interface here and with the other source is a bit fragile
+    you need to first call needs_reload
+    and then if true, you need to call get_all_events
+    otherwise call get_new_events
+    needs_reload says only if it needs reloading since last time you called needs_reload
+    not since last time you did any of get_*_events
+    """
+
+    def __init__(self, path: Path):
+        self.path = path
+        self.archived = ArchivedSources(path / "archive")
+        self.active = ActiveSources(path)
+
+    def needs_reload(self):
+        # TODO very brittle :/ see class doc, maybe there is a more robust way without much code?
+        # otherwise call it check instead of state-y?
+        archived = self.archived.needs_reload()
+        active = self.active.needs_reload()
+        return archived or active
+
+    def get_all_events(self):
+        archived_osh, archived_other = self.archived.get_all_events()
+        active_osh = self.active.get_all_events()
+        events = merge_other_into_main(archived_other, archived_osh + active_osh)
+        return events
+
+    def get_new_events(self):
+        return self.active.get_new_events()
+
+
+@dataclass(frozen=True)
+class DiscoveredArchiveFile:
+    path: Path
+    size: int
+    mtime: float
+
+    @classmethod
+    def from_path(cls, path: Path):
+        stat = path.stat()
+        return cls(path, stat.st_size, stat.st_mtime)
+
+
+class ArchivedSources:
+    def __init__(self, path: Path):
+        self.path = path
+        self.last_check = -math.inf
+        self.min_delay = 10
+        self.files = None
+
+    def needs_reload(self):
+        now = time.time()
+        if now - self.last_check < self.min_delay:
+            return False
+        self.last_check = now
+        files = self.discover_files()
+        if files == self.files:
+            return False
+        self.files = files
+        return True
+
+    def get_all_events(self):
+        osh, other = [], []
+        for dfile in self.files:
+            try:
+                if dfile.path.suffix == ".osh":
+                    osh.extend(read_osh_file(dfile.path))
+                elif dfile.path.suffix == ".osh_legacy":
+                    osh.extend(read_osh_legacy_file(dfile.path))
+                elif dfile.path.suffix == ".zsh_history":
+                    other.extend(read_zsh_file(dfile.path))
+                else:
+                    raise Exception(f"unknown type of history {dfile.path}")
+            except FileNotFoundError:
+                pass
+        return osh, other
+
+    def discover_files(self):
+        glob = itertools.chain(
+            self.path.glob("**/*.osh"),
+            self.path.glob("**/*.osh_legacy"),
+            self.path.glob("**/*.zsh_history"),
+        )
+        return {DiscoveredArchiveFile.from_path(p) for p in glob}
+
+
+class ActiveSources:
+    def __init__(self, path: Path):
+        self.path = path
+        self.last_check = -math.inf
+        self.min_delay = 10
+        self.files = None
+
+    def needs_reload(self):
+        now = time.time()
+        if now - self.last_check < self.min_delay:
+            return False
+        self.last_check = now
+        files = self.discover_files()
+        if files == self.files:
+            return False
+        self.files = files
+        return True
+
+    def get_all_events(self):
+        self.readers = [OshFileReader(f) for f in self.files]
+        return list(self.get_new_events())
+
+    def get_new_events(self):
+        for reader in self.readers:
+            yield from reader.read_events()
+
+    def discover_files(self):
+        return set(self.path.glob("*.osh"))
 
 
 class Source:
@@ -34,24 +151,7 @@ class Source:
         raise NotImplementedError()
 
 
-class UnionSource(Source):
-    """
-    do not change the list of sources afterwards
-    instead make a new UnionSource
-    """
-
-    def __init__(self, sources: list[Source]):
-        self.ctime = time.time()
-        self.sources = sources
-
-    def as_list(self) -> list[Event]:
-        return [event for source in self.sources for event in source.as_list()]
-
-    def mtime(self):
-        return max(self.ctime, max(s.mtime() for s in self.sources))
-
-
-class MergeInSource(Source):
+def merge_other_into_main(other, main):
     """
     we generally assume that the 'main' source has no collisions with itself
     typically 'main' comes from osh sources, and you dont run multiple osh's on the same machine in parallel
@@ -60,157 +160,34 @@ class MergeInSource(Source):
     in short, duplicates within 'main' are not dealt with, but duplicates within 'other' and against 'main' are dealt with
     """
 
-    def __init__(self, main: Source, other: Source):
-        self.main = main
-        self.other = other
+    if len(other) == 0:
+        return main
 
-    def as_list(self):
+    # zsh and bash seem to use a posix timestamp floored to seconds
+    # therefore these are the seconds when 'main' was recording
+    # and we dont need to use 'other' to complete our history
+    # (this is a somehwat crude heuristics in order to be efficient)
+    covered_seconds = {math.floor(event.timestamp.timestamp()) for event in main}
 
-        events = self.main.as_list()
-        candidates = self.other.as_list()
+    # TODO some easy stats here to see if there is much after coming in, or restrict to last 10 days
+    # but how to warn, or return stats, or handle it with user notification?
+    # print(f"{min(events_seconds)=}")
+    # print(f"{len(merge_events)=}")
+    additional = [
+        event
+        for event in other
+        if math.floor(event.timestamp.timestamp()) not in covered_seconds
+    ]
+    # print(f"{len(merge_events)=}")
+    # merge_events_after = [event for event in merge_events if math.floor(event.timestamp.timestamp())>min(events_seconds)]
+    # print(f"{len(merge_events_after)=}")
 
-        if len(candidates) == 0:
-            return events
-
-        # zsh and bash seem to use a posix timestamp floored to seconds
-        # therefore these are the seconds when 'main' was recording
-        # and we dont need to use 'other' to complete our history
-        # (this is a somehwat crude heuristics in order to be efficient)
-        covered_seconds = {math.floor(event.timestamp.timestamp()) for event in events}
-
-        # TODO some easy stats here to see if there is much after coming in, or restrict to last 10 days
-        # but how to warn, or return stats, or handle it with user notification?
-        # print(f"{min(events_seconds)=}")
-        # print(f"{len(merge_events)=}")
-        additional = [
-            event
-            for event in candidates
-            if math.floor(event.timestamp.timestamp()) not in covered_seconds
-        ]
-        # print(f"{len(merge_events)=}")
-        # merge_events_after = [event for event in merge_events if math.floor(event.timestamp.timestamp())>min(events_seconds)]
-        # print(f"{len(merge_events_after)=}")
-
-        return events + additional
-
-    def mtime(self):
-        return max(self.main.mtime, self.other.mtime)
-
-
-class OshSource(Source):
-    def __init__(self, path: Path):
-        self.path = path
-
-    def as_list(self) -> list[Event]:
-        try:
-            return read_osh_file(self.path)
-        except FileNotFoundError:
-            return []
-
-    def mtime(self):
-        try:
-            return self.path.stat().st_mtime
-        except FileNotFoundError:
-            return time.time()
-
-
-class IncrementalOshSource(Source):
-    def __init__(self, path: Path):
-        self.path = path
-        self.reader = None
-        self.events = None
-
-    def as_list(self):
-        if self.reader is None:
-            self.reader = OshFileReader(self.path)
-            self.events = []
-
-        try:
-            self.events.extend(self.reader.read_events())
-            return self.events
-
-        except FileNotFoundError:
-            self.reader = None
-            self.events = []
-            return self.events
-
-        except FileChangedMuch:
-            self.reader = None
-            self.events = None
-            return self.as_list()
-
-    def mtime(self):
-        try:
-            return self.path.stat().st_mtime
-        except FileNotFoundError:
-            return time.time()
-
-
-class OshLegacySource(Source):
-    def __init__(self, path: Path):
-        self.path = path
-
-    def as_list(self):
-        try:
-            return read_osh_legacy_file(self.path)
-        except FileNotFoundError:
-            # TODO i'm not sure now, return [] or last data here? [] would be probably better
-            return []
-
-    def mtime(self):
-        try:
-            return self.path.stat().st_mtime
-        except FileNotFoundError:
-            return time.time()
-
-
-class ZshSource(Source):
-    def __init__(self, path: Path):
-        self.path = path
-
-    def as_list(self):
-        try:
-            return read_zsh_file(self.path)
-        except FileNotFoundError:
-            return []
-
-    def mtime(self):
-        try:
-            return self.path.stat().st_mtime
-        except FileNotFoundError:
-            return time.time()
-
-
-class CachedSource(Source):
-    def __init__(self, source: Source, min_delay: float = 5.0):
-        self.source = source
-        self.min_delay = min_delay
-        self.check_time = time.time()
-        self.cached_mtime = time.time()
-        self.cached_events = self.source.as_list()
-
-    def as_list(self):
-        self.maybe_refresh()
-        return self.cached_events
-
-    def mtime(self):
-        self.maybe_refresh()
-        return self.cached_mtime
-
-    def maybe_refresh(self):
-        if time.time() - self.check_time < self.min_delay:
-            return
-        mtime = self.source.mtime()
-        if mtime == self.cached_mtime:
-            return
-        self.cached_mtime = mtime
-        self.cached_events = self.source.as_list()
+    return main + additional
 
 
 if __name__ == "__main__":
-    sources = [OshLegacySource()]
-    merge_sources = [ZshSource()]
-    source = UnionSource(sources, merge_sources)
-    events = source.as_list()
+    source = IncrementalSource(Path("histories"))
+    print(f"{source.needs_reload()=}")
+    events = source.get_all_events()
     print(f"{len(events)=}")
     print(f"{events[-1]=}")
