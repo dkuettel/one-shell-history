23b1fafd3247e11a3d8f82a832049337e8eac5c5
Daniel Kuettel <dkuettel@gmail.com>
2021-10-16 16:19:52 +0200

    add some defaults

---
+++ python/osh/defaults.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+# TODO dot = Path("~/.one-shell-history").expanduser()
+dot = Path(".").expanduser()
+histories = Path("histories")
+event_filters = Path("event-filters.yaml")
+local = Path("local.osh")
