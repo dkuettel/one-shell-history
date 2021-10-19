e28fe7ba0e50c670c639a1eff74be4e26cf2ab18
Daniel Kuettel <dkuettel@gmail.com>
2021-10-19 20:22:12 +0200

    support ctrl-p and ctrl-n in zsh

--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -1,19 +1,19 @@
 # source this (eg, in zshrc) to add one-shell-history functionality
 
 # TODO we dont require it in the path, up to the user if he wants to call 'osh stats' and the like
-__osh=$(realpath ${0:a:h}/../bin/osh)
+# on the other hand, we need it ready here anyway, so why not just make it available? difference of shell function vs script?
+# not sure, still nice to let the user decide
+# but then would also be nice to say it needs to be available some way, we dont discover it?
+# but then we might not use the one of this script, but another one, a bit messy
+__osh_path=$(realpath ${0:a:h}/../bin/osh)
 
 function __osh {
     $__osh_path $@
 }
 
-if [[ ! -v __osh_session_id ]]; then
-    __osh_session_id=$(uuidgen)
-fi
-
-if [[ ! -v __osh_session_start ]]; then
-    __osh_session_start=$(date '+%s.%N')
-fi
+__osh_session_id=$(uuidgen)
+__osh_session_start=$(date '+%s.%N')
+__osh_prefix_timestamp=$(date '+%s.%N')
 
 autoload -U add-zsh-hook
 
@@ -40,6 +40,7 @@ function __osh_after {
         __osh append-event $__osh_current_command &!
         unset __osh_current_command
     fi
+    __osh_prefix_timestamp=$(date '+%s.%N')
 }
 
 add-zsh-hook zshaddhistory __osh_before
@@ -66,3 +67,31 @@ zle -N __osh_search_backwards
 bindkey '^e' __osh_search_backwards
 bindkey -M vicmd '^e' __osh_search_backwards
 bindkey -M viins '^e' __osh_search_backwards
+
+function __osh_previous {
+    # NOTE --ignore=$BUFFER would skip consecutive duplicates, sounds good, but not typically intuitive
+    if result=$(__osh previous-event --timestamp=$__osh_prefix_timestamp --prefix=$BUFFER[1,$CURSOR] --session-id=$__osh_session_id --session-start=$__osh_session_start); then
+        __osh_prefix_timestamp=$result[1,21]
+        BUFFER=$result[23,-1]
+    fi
+    zle reset-prompt
+}
+
+zle -N __osh_previous
+bindkey '^p' __osh_previous
+bindkey -M vicmd '^p' __osh_previous
+bindkey -M viins '^p' __osh_previous
+
+function __osh_next {
+    # NOTE --ignore=$BUFFER would skip consecutive duplicates, sounds good, but not typically intuitive
+    if result=$(__osh next-event --timestamp=$__osh_prefix_timestamp --prefix=$BUFFER[1,$CURSOR] --session-id=$__osh_session_id --session-start=$__osh_session_start); then
+        __osh_prefix_timestamp=$result[1,21]
+        BUFFER=$result[23,-1]
+    fi
+    zle reset-prompt
+}
+
+zle -N __osh_next
+bindkey '^n' __osh_next
+bindkey -M vicmd '^n' __osh_next
+bindkey -M viins '^n' __osh_next
