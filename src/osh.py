12e772cce587ed9c6de79856cfc9095a8b6412ab
Daniel Kuettel <dkuettel@gmail.com>
2025-10-10 16:48:18 +0200

    correctly resize

--- src/osh.py
+++ src/osh.py
@@ -91,11 +91,14 @@ def insert_osh_event(event: Event, path: Path, lock: bool):
         data = encoder.encode(event)
         size = len(data)
 
-        if insert_at < mm.size():
+        shift_size = mm.size() - insert_at
+        mm.resize(mm.size() + size + 2)
+
+        if shift_size > 0:
             mm.move(
                 dest=insert_at + size + 2,
                 src=insert_at,
-                count=mm.size() - insert_at,
+                count=shift_size,
             )
 
         size_bytes = size.to_bytes(length=2, byteorder="big", signed=False)
